0a9c72123cfe54d033739f40e01904f39951e53e
Felipe Lundgren <felipec5l2@hotmail.com>
2025-05-05 17:04:33 -0300

    C: destrinchando os dados

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ print(response)
 
 if response.status_code == 200:
     dados_json = response.json()
-    print(dados_json)
+    dados_restaurante = {}
+    for i in dados_json:
+        nome_do_restaurante = i['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+        
+        dados_restaurante[nome_do_restaurante].append({
+            'item':i['Item'],
+            'price': i['price'],
+            'description': i['description']
+            })
 else:
     print(f'O erro foi {response.status_code}')
+
+print(dados_restaurante['McDonald’s'])
